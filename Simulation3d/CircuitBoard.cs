a1ace0b67026f54b285eee527768ac05271ffd85
alexeysp11 <alexeyspiridon1111@gmail.com>
2021-03-30 10:03:45 +0500

    Rotation angle varies from -360 to 360 degrees

--- Simulation3d/CircuitBoard.cs
+++ Simulation3d/CircuitBoard.cs
@@ -51,21 +51,64 @@ namespace Simulation3d
             return _Angle; 
         }
 
-        public void SetRotation(float dx, float dy, float dz)
+        /// <summary>
+        /// Sets values of an instance of Angle structure. 
+        /// Adds deltas to rotation angle around each of axis and corrects
+        //// the values if they are less than -360 or bigger than 360. 
+        /// </summary>
+        /// <param name="dx">
+        /// Delta for rotation around X axis (by default is equal to zero). 
+        /// </param>
+        /// <param name="dy">
+        /// Delta for rotation around Y axis (by default is equal to zero). 
+        /// </param>
+        /// <param name="dz">
+        /// Delta for rotation around Z axis (by default is equal to zero). 
+        /// </param>
+        public void SetRotation(float dx = 0, float dy = 0, float dz = 0)
         {
             _Angle.X += dx; 
             _Angle.Y += dy; 
             _Angle.Z += dz; 
+
+            if (_Angle.X >= 360)
+            {
+                _Angle.X -= 360; 
+            }
+            else if (_Angle.X <= -360)
+            {
+                _Angle.X += 360; 
+            }
+
+            if (_Angle.Y >= 360)
+            {
+                _Angle.Y -= 360; 
+            }
+            else if (_Angle.Y <= -360)
+            {
+                _Angle.Y += 360; 
+            }
+
+            if (_Angle.Z >= 360)
+            {
+                _Angle.Z -= 360; 
+            }
+            else if (_Angle.Z <= -360)
+            {
+                _Angle.Z += 360; 
+            }
         }
 
-        public void GetAcceleration()
+        public Acceleration GetAcceleration()
         {
-
+            return _Accel; 
         } 
 
-        public void SetAcceleration()
+        public void SetAcceleration(float dx = 0, float dy = 0, float dz = 0)
         {
-
+            _Accel.X += dx; 
+            _Accel.Y += dy; 
+            _Accel.Z += dz;
         } 
         
         #endregion  // Methods
